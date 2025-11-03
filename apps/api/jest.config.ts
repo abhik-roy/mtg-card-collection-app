ec9c569f9899142f7ffc0cc2e742c1ed49956551
Abhik Roy <33581435+abhik-roy@users.noreply.github.com>
2025-11-03 18:19:43 -0500

    fixed tests

--- apps/api/jest.config.ts
+++ apps/api/jest.config.ts
@@ -3,6 +3,7 @@ import type { Config } from 'jest';
 const config: Config = {
   roots: ['<rootDir>/src', '<rootDir>/test'],
   testMatch: ['**/?(*.)+(spec|test|e2e-spec).[tj]s?(x)'],
+  testTimeout: 30000,
   transform: {
     '^.+\\.(t|j)sx?$': [
       'ts-jest',
